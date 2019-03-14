1a7368aa375d6caf8e3575570a0147507a2ae15f
VHugoBarnes <hugovg799@gmail.com>
2019-03-13 20:56:48 -0500

    13/03/2019 Asincronismo

--- clase6.js
+++ clase6.js
@@ -1,34 +1,53 @@
 //HERENCIA PROTOTIPAL
-function heredaDe(prototipoHijo, prototipoPadre){
-    var fn = function(){}
-    fn.prototype = prototipoPadre.prototype
-    prototipoHijo.prototype = new fn()
+// function heredaDe(prototipoHijo, prototipoPadre){
+//     var fn = function(){}
+//     fn.prototype = prototipoPadre.prototype
+//     prototipoHijo.prototype = new fn()
+// }
+/////////////////////////////////////////////////////////////////////////////////////////
+
+class Persona {
+
+    constructor(nombre, apellido, altura){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.altura = altura
+    }
+    saludar(fn){
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+        if(fn){
+            fn(this.nombre, this.apellido, false)
+        }
+    }
+    soyAlto(){
+        return this.altura > 1.8
+    }
 }
 
-function Persona(nombre, apellido, altura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
+class Desarrollador extends Persona{
+    constructor(nombre, apellido,altura){
+        super(nombre, apellido,altura)
+    }
+    saludar(fn){
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+        if(fn){
+            fn(this.nombre, this.apellido, true)
+        }
+    }
 }
 
-Persona.prototype.saludar = function (){
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+function responderSaludo(nombre, apellido, esDev){
+    console.log(`Buen día ${nombre} ${apellido}`)
+
+    if(esDev){
+        console.log(`Ah mirá, no sabía que eras desarrollador/a`)
+    }
 }
 
-Persona.prototype.soyAlto = function() { 
-    return this.altura > 1.8
-}
+ var victor = new Persona('Víctor', 'Vázquez',1.7)
+ var keko = new Persona('Keko', 'Kaka', 1.9)
+ var reba = new Desarrollador('Reba', 'Meyers', 1.6)
 
-function Desarrollador(nombre, apellido){
-    this.nombre = nombre
-    this.apellido = apellido
-}
-
-Desarrollador.prototype.saludar = function(){
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
-}
-
-heredaDe(Desarrollador, Persona)
-
-// var victor = new Persona('Víctor', 'Vázquez',1.7)
-// victor.saludar()
+ victor.saludar()
+ keko.saludar(responderSaludo)
+ reba.saludar(responderSaludo)
