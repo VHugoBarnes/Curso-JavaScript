f4e6dd710ae3ea379b52cc7bba9a9ea80be2e48a
VHugoBarnes <hugovg799@gmail.com>
2019-03-16 18:44:40 -0500

    Iniciando el juego

--- clase7.js
+++ clase7.js
@@ -7,30 +7,53 @@ const PEOPLE_URL = 'people/:id'
 const OPTS ={ crossDomain:true }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-function obtenerPersonaje(id, callback){
-
-    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-
-    $.get(url, OPTS, callback).fail(function(){
-        console.log(`Sucedio un error, no se pudo obtener el personaje ${id}`)
+function obtenerPersonaje(id){
+    
+    //PROMESA
+    return new Promise((resolve, reject) =>{
+        const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+        $.get(url, OPTS, function(data){
+            resolve(data)
+        }).fail(() => reject(id))
     })
+
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-obtenerPersonaje(1, function(persona){
-    console.log(`Hola, yo soy  ${persona.name}`)
-
-    obtenerPersonaje(2, function(persona){
-        console.log(`Hola, yo soy  ${persona.name}`)
-
-        obtenerPersonaje(3, function(persona){
-            console.log(`Hola, yo soy  ${persona.name}`)
-
-            obtenerPersonaje(4, function(persona){
-                console.log(`Hola, yo soy  ${persona.name}`)
-            })
-        })
-    })
-})
+function onError(id){
+    console.log(`Sucedió un error al obtener el personaje ${id}`)
+}
 
 
+// var promesas = ids.map(function (id) {
+//     return obtenerPersonaje(id)
+// })
+
+async function obtenerPersonajes() {
+    var ids = [1, 2, 3, 4, 5, 6, 7]
+    var promesas = ids.map(id =>  obtenerPersonaje(id))
+
+    try {
+        var personajes = await Promise.all(promesas)
+        console.table(personajes)
+    } catch (id) {
+        onError(id)
+    } 
+}
+
+obtenerPersonajes()
+
+// obtenerPersonaje(1)
+//     .then(personaje1 =>{
+//         console.log(`El personaje  1 es: ${personaje1.name}`)
+//         return obtenerPersonaje(2)    
+//     })
+//     .then(personaje2 =>{
+//         console.log(`El personaje  1 es: ${personaje2.name}`)
+//         return obtenerPersonaje(3)    
+//     })
+//     .then(personaje3 =>{
+//         console.log(`El personaje  3 es: ${personaje3.name}`)
+//         // return obtenerPersonaje(2)    
+//     })
+// .catch(onError)
